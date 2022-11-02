0b323fa430b79d285fe68a955869a1f787acc26c
Fabian De Gouveia <fabiandegouveia@gmail.com>
2022-11-02 13:44:16 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title: string = 'my-first-app';
+  welcome: string = 'Gran HP';
+  isRunning: boolean = true;
+  message: string [] = []
+  people: {firstName:string, lastName:string, age:number}[] = [
+  {firstName: 'Fabian', lastName: 'De Gouveia', age: 26},
+  {firstName: 'Wildys', lastName: 'De Gouveia', age: 24},
+  {firstName: 'Tayra', lastName: 'De Gouveia', age: 1},
+  {firstName: 'Nose', lastName: 'De Gouveia', age: 2}
+  ]
+
+  url:string = 'http://localhost:2992/api/getSpecieDetails'
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get<any>(this.url).subscribe(data =>{
+      console.log(data);
+      
+    })
+  }
+  testFunction(value:string):void{
+    console.log(`testing Fabian ${value}`);
+    this.message.push('test function called!')
+    
+  }
 }
